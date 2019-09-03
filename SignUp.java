0a98c82c18b936f66cc263cc999a96559303b9a8
Caleb Schroder <doomskier@gmail.com>
2019-09-03 12:53:04 -0600

    signup

---
+++ SignUp.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class SignUp {
+
+    public static void main(String[]args){
+
+        Scanner scan = new Scanner(System.in);
+
+        String password;
+        String username;
+        
+
+        System.out.println("Sign up for nerds.com, please");
+        System.out.println("Input Password");
+        password = scan.nextLine();
+        System.out.println("Input Username");
+        username = scan.nextLine();
+
+       
+        
+
+        if (username.equals("person")) {
+            System.out.println("Username is Unavailable");
+        }
+        if (username.equals("admin")) {
+            System.out.println("Username is Unavailable");
+        }
+        if (username.equals("jimbob234")) {
+            System.out.println("Username is Unavailable");
+        }
+        if (username.equals("skater_gurl17")) {
+            System.out.println("Username is Unavailable");
+        }
+        if (username.equals("gary")) {
+            System.out.println("Username is Unavailable");
+        }
+       
+        
+       
+
+        
+
+
+        
+      
+        
+
+
+
+    
+    }   
+
+
+}
