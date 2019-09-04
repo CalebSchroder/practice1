673a2135dd097b38a09481fc1762e65db38c848e
Caleb Schroder <doomskier@gmail.com>
2019-09-04 13:08:54 -0600

    Signup almost done

--- SignUp.java
+++ SignUp.java
@@ -7,12 +7,12 @@ public class SignUp {
 
         String password;
         String username;
+        int password2;
         
 
         System.out.println("Sign up for nerds.com, please");
-        System.out.println("Input Password");
-        password = scan.nextLine();
-        System.out.println("Input Username");
+        
+         System.out.println("Input Username");
         username = scan.nextLine();
 
        
@@ -33,8 +33,37 @@ public class SignUp {
         if (username.equals("gary")) {
             System.out.println("Username is Unavailable");
         }
-       
+
         
+        System.out.println("Input Password");
+        password = scan.nextLine();
+        
+        
+        if (password.startsWith("a") || password.startsWith("e") || password.startsWith("i") || password.startsWith("o") || password.startsWith("u")) {
+        
+        } else {
+            System.out.println("Password Unavailabe");
+        }
+
+
+        if (password.contains("#") || password.contains("!") || password.contains("$") || password.contains("*") || password.contains("&")) {
+
+        } else {
+            System.out.println("Password Unavailable");
+        }
+
+
+        if(password.contains(username)){
+            System.out.println("Password Unavailable");
+        }
+        
+        password2 = password.length() - 2;
+        String password3 = password.substring(password2);
+
+        if(password3.contains("1") || password3.contains("2") || password3.contains("3") || password3.contains("4") || password3.contains("5") || password3.contains("6") || password3.contains("7") || password3.contains("8") || password3.contains("9")){
+            System.out.println("Password Unavailable");
+        }
+ 
        
 
         
